b1735672bc39e6971e2f428fae6c9da676410050
a.vasilyev <asv2108@gmail.com>
2023-03-21 14:22:49 +0200

    ADD computed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- const App = {
+const App = {
     data() {
         return {
             placeholderMy: 'Enter text here',
@@ -33,6 +33,18 @@
             //(idx, event) если нужен еще и event browser, то в html в функцию передаем $event
             //но возможно он передается по умолчанию и в html в функцию передавать не нужно?
             this.notes.splice(idx, 1)
+        },
+        //будет вызвано каждый раз при взаимодействии с this.notes а это потеря производительности,
+        // вместо этого нужно использовать computed
+        // doubleCount() {
+        //     return this.notes.length * 2;
+        // }
+    },
+    computed: {
+        // данные методы обязаны что то возвращать и зависят от переменнных вычисляемых результат в приложения
+        // обращаемя к ним через геттеры и сеттеры (без круглых скобок) как к переменным
+        doubleCountComputed() {
+            return this.notes.length * 2;
         }
     }
 }
